7fc1ae0a2a76c3b65587a0421f74a9c289ad76e7
Izabella Szlósarczyk <iza.bella.szlosarczyk@gmail.com>
2020-11-10 18:37:45 +0100

    Add form and list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="container-fluid">
+   <nav class="navbar navbar-light bg-light">
+    <span class="navbar-brand mb-0 h1">Participants</span>
+  </nav>
+  <div class="app-content">
+    <div class="app-list">
+      <h1>List</h1>
+      <div v-if="participants.length > 0" class="app-list-constent">
+        <div v-for="participant in participants" v-bind:key="participant.id">
+          {{ participant.name }} {{ participant.surname}} 
+          <button v-on:click="onRemove(participant)" class="btn btn-danger" type="button">Remove</button>
+        </div>
+      </div>
+      <div class="alert alert-warning" role="alert" v-else>
+        Noone here :c 
+      </div>
+    </div>
+    <div class="app-add-form">
+      <form v-on:submit.prevent="onSubmit()">
+        <h1>Add participant</h1>
+        <div class="form-group">
+          <label for="participantName">Name</label>
+          <input v-model="newParticipantName" type="text" class="form-control" id="participantName" placeholder="Fill name">
+        </div>
+        <div class="form-group">
+          <label for="participantSurname">Surname</label>
+          <input v-model="newParticipantSurname" type="text" class="form-control" id="participantSurname" placeholder="Fill surname">
+        </div>
+        <button class="btn btn-info" type="submit">Submit</button>
+      </form>
+    </div>
   </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data: function () {
+      return {
+        participants: [],
+        newParticipantName: '',
+        newParticipantSurname: '',
+      }
+    },
+    methods: {
+      onSubmit: function (){
+        const newParticipant = {
+          name: this.newParticipantName,
+          surname: this.newParticipantSurname,
+          id: Math.random(),
+        }
+        this.participants.push(newParticipant);
+        this.newParticipantSurname = '';
+        this.newParticipantName = '';
+      },
+      onRemove: function (participantToRemove){
+        this.participants = this.participants.filter((participant) => {
+          return participant.id !== participantToRemove.id;
+        });
+      }
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container-fluid {
+  margin: 0;
+  padding: 0;
+}
+
+.app-content {
+  margin: 30px;
 }
 </style>
